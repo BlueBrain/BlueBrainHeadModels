071ce5b058bda148817b98fc7ba07539d5a51884
Tharayil Joseph <joseph.tharayil@epfl.ch>
2024-03-30 17:26:47 +0100

    For some reason a weight of 6 works but 8 rotates the head

--- code/eval_v2.py
+++ code/eval_v2.py
@@ -23,7 +23,7 @@ paxlabels = ants.crop_image(paxlabels,label_image=binarymask)
 sscx = np.array([721,722,723,724,725,726,727,728,730,731])
 
 newvalscopy = im.numpy()
-newvalscopy[np.where(~np.isin(newvalscopy,sscx))]=8
+newvalscopy[np.where(~np.isin(newvalscopy,sscx))]=6
 newvalscopy[np.where(np.isin(newvalscopy,sscx))]=11 # Sets  Sscx to 1
 
 newvalscopy[np.where(newvals==0)]=0 # Sets background to 0
@@ -32,9 +32,9 @@ newvalscopy[np.where(newvals==0)]=0 # Sets background to 0
 sscxmask = im.new_image_like(newvalscopy)
 newerimage = ants.crop_image(sscxmask,label_image=binarymask)
 
-ants.image_write(binarymask,'../intermediateFiles/paxLabelsAlignedToOsparcBinaryMask.nii.gz')
-ants.image_write(paxlabels,'../intermediateFiles/paxLabelsAlignedToOsparcMasked.nii.gz') # Osparc rat with paxinos-watson labels, with mask applied
-ants.image_write(newerimage,'../intermediateFiles/paxLabelsAlignedToOsparcMask.nii.gz') # Mask of osparc rat
+#ants.image_write(binarymask,'../intermediateFiles/paxLabelsAlignedToOsparcBinaryMask.nii.gz')
+#ants.image_write(paxlabels,'../intermediateFiles/paxLabelsAlignedToOsparcMasked.nii.gz') # Osparc rat with paxinos-watson labels, with mask applied
+ants.image_write(newerimage,'../intermediateFiles/paxLabelsAlignedToOsparcMask_v2.nii.gz') # Mask of osparc rat
 
 im = ants.image_read('../data/PW_RBSC_6th_indexed_volume.nii.gz') #Paxinos-watson atlas
 
@@ -47,12 +47,12 @@ newvalscopy = im.numpy()
 newvalscopy[np.where(newvalscopy!=0)]=1 
 binarymask = im.new_image_like(newvalscopy)
 
-newvals[np.where(~np.isin(newvals,sscx))]=8
+newvals[np.where(~np.isin(newvals,sscx))]=6
 newvals[np.where(np.isin(newvals,sscx))]=11 # Sets sscx to 1
 newvals[np.where(newvalscopy==0)]=0
 
 newimage = im.new_image_like(newvals)
 
-ants.image_write(newimage,'../intermediateFiles/PWMaskMask.nii.gz') # Mask of paxinos-watson atlas
-ants.image_write(binarymask,'../intermediateFiles/PWMaskBinary.nii.gz') # Mask of paxinos-watson atlas
+ants.image_write(newimage,'../intermediateFiles/PWMaskMask_v2.nii.gz') # Mask of paxinos-watson atlas
+#ants.image_write(binarymask,'../intermediateFiles/PWMaskBinary.nii.gz') # Mask of paxinos-watson atlas
 
