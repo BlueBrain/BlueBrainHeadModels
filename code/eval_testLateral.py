160e031115695d0ccba1c13c845ff4af7ac600c8
Tharayil Joseph <joseph.tharayil@epfl.ch>
2024-03-31 18:30:20 +0200

    Adds laterality

---
+++ code/eval_testLateral.py
@@ -0,0 +1,61 @@
+import ants
+import numpy as np
+import sys
+
+#baseline = int(sys.argv[1])
+
+im = ants.image_read('../intermediateFiles/paxLabelsAlignedToOsparc.nii.gz') # OSPARC atlas with paxinos-watson labels, created by writeRegionsPW.py
+
+
+newvals = im.numpy()
+
+
+newvals[np.where(newvals==866)]=0 # Sets background to 0
+
+
+paxlabels = im.new_image_like(newvals)
+
+newvals[np.where(newvals!=0)]=1
+
+
+binarymask = im.new_image_like(newvals)
+paxlabels = ants.crop_image(paxlabels,label_image=binarymask)
+
+
+sscx = np.array([721,722,723,724,725,726,727,728,730,731])
+
+newvalscopy = im.numpy()
+newvalscopy[np.where(~np.isin(newvalscopy,sscx))]=6
+newvalscopy[np.where(np.isin(newvalscopy,sscx))]=11 # Sets  Sscx to 1
+newvalscopy[:,520:,:]=6
+newvalscopy[np.where(newvals==0)]=0 # Sets background to 0
+
+
+sscxmask = im.new_image_like(newvalscopy)
+newerimage = ants.crop_image(sscxmask,label_image=binarymask)
+
+#ants.image_write(binarymask,'../intermediateFiles/paxLabelsAlignedToOsparcBinaryMask.nii.gz')
+#ants.image_write(paxlabels,'../intermediateFiles/paxLabelsAlignedToOsparcMasked.nii.gz') # Osparc rat with paxinos-watson labels, with mask applied
+ants.image_write(newerimage,'../intermediateFiles/paxLabelsAlignedToOsparcMask_lateral.nii.gz') # Mask of osparc rat
+
+#im = ants.image_read('../data/PW_RBSC_6th_indexed_volume.nii.gz') #Paxinos-watson atlas
+
+
+#newvals = im.numpy()
+
+
+#newvalscopy = im.numpy()
+
+#newvalscopy[np.where(newvalscopy!=0)]=1 
+#binarymask = im.new_image_like(newvalscopy)
+
+#newvals[np.where(~np.isin(newvals,sscx))]=baseline
+#newvals[np.where(np.isin(newvals,sscx))]=11 # Sets sscx to 1
+#newvals[0:int(newvals.shape[0]/2),:,:]=baseline # Sets sscx to 1
+#newvals[np.where(newvalscopy==0)]=0
+
+#newimage = im.new_image_like(newvals)
+
+#ants.image_write(newimage,'../intermediateFiles/PWMaskMask_v'+str(baseline)+'.nii.gz') # Mask of paxinos-watson atlas
+#ants.image_write(binarymask,'../intermediateFiles/PWMaskBinary.nii.gz') # Mask of paxinos-watson atlas
+
